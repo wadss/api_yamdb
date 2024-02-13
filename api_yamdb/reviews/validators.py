e69313475af01130d59f2bfbf00062fdd3d58660
Daniyar Alzhanov <Alorealdo@yandex.ru>
2024-02-13 19:07:09 +0300

    Stage commit

---
+++ api_yamdb/reviews/validators.py
@@ -0,0 +1,11 @@
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+
+def validate_year(year):
+    """Проверка на превышение текущего года."""
+    current_year = timezone.now().year
+    if year > current_year:
+        raise ValidationError(
+            'Некорректно указан год. '
+            f'Значение не может превышать: {current_year}'
+        )
---
+++ api_yamdb/users/models.py
@@ -0,0 +1,69 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+ROLE_CHOICES = [
+    ('user', 'Пользователь'),
+    ('moderator', 'Модератор'),
+    ('admin', 'Администратор')
+]
+
+
+class User(AbstractUser):
+    """Модель пользователя."""
+
+    ADMIN = 'admin'
+    MODERATOR = 'moderator'
+    USER = 'user'
+
+    username = models.CharField(
+        verbose_name='Никнейм пользователя',
+        max_length=150,
+        unique=True,
+        null=False,
+    )
+    email = models.EmailField(
+        verbose_name='Электронная почта',
+        max_length=254,
+        unique=True,
+    )
+    first_name = models.CharField(
+        verbose_name='Имя пользователя',
+        max_length=150,
+        blank=True
+    )
+    last_name = models.CharField(
+        verbose_name='Фамилия пользователя',
+        max_length=150,
+        blank=True,
+    )
+    bio = models.TextField(
+        verbose_name='Биография пользователя',
+        blank=True,
+    )
+    role = models.CharField(
+        verbose_name='Роль пользователя',
+        choices=ROLE_CHOICES,
+        default=ROLE_CHOICES[0][0],
+        max_length=10,
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('id',)
+    
+    def __str__(self):
+        return self.username
+    
+    @property
+    def is_moderator(self):
+        return self.role == self.MODERATOR
+    
+    @property
+    def is_admin(self):
+        return self.role == self.ADMIN or self.is_superuser
+    
+    @property
+    def is_user(self):
+        return self.role == self.USER
