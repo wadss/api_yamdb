fbff784e714d011b3e1db8ffa9749fc7880ac457
WaDss <wadss07@mail.ru>
2024-02-18 22:45:46 +0300

    fnl

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -18,7 +18,6 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    'rest_framework_simplejwt',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
